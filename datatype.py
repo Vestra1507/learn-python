3bd0bb9452d67f722332ed1642b4fa952fc78d24
Rachata Sangeamjai <Rachata.sang@outlook.com>
2026-06-24 20:37:05 +0700

    list

---
+++ datatype.py
@@ -0,0 +1,25 @@
+# #list
+# product=["กางเกง",99.99,10,True]
+# product1=list(("plant",99.10,10,False))
+# print(len(product1))
+# #edit list
+# product[0]="shirt"
+
+
+# #การเข้าถึงสมาชิก
+# print(product[0])
+
+# #Colors
+# colors1=["black","red","green","black"]
+# colors2=["white","blue","orange"]
+# fullcolors=colors1+colors2
+# print(fullcolors)
+# print(type(fullcolors))
+
+# colors=["red","green","blue","black","white"]
+# print(colors[0:3])
+
+colors=["red","green","blue","black","white"]
+colors.append("brown") #เพิ่ม 1 รายการ
+colors.extend(["orange","yellow"])
+print(colors)
