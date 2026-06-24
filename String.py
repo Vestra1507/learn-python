5d187d7de212647b5c8aa552aa1aa4dc301898b9
Rachata Sangeamjai <Rachata.sang@outlook.com>
2026-06-24 11:48:58 +0700

    add sting

---
+++ String.py
@@ -0,0 +1,16 @@
+#deep string
+fname = "Rachata "
+Lname = " Sangeamjai "
+
+fullname = fname+Lname +" Student"
+print(fullname)
+
+address = """
+99/1 moo.7
+Thubma Muang
+Rayong 
+21000
+Thailand
+"""
+
+print(address)
