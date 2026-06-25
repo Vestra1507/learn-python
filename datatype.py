d9597330bc350a8f1a31f743e136d76f176ed396
Rachata Sangeamjai <Rachata.sang@outlook.com>
2026-06-25 14:51:17 +0700

    datatype

--- datatype.py
+++ datatype.py
@@ -19,7 +19,78 @@
 # colors=["red","green","blue","black","white"]
 # print(colors[0:3])
 
-colors=["red","green","blue","black","white"]
-colors.append("brown") #เพิ่ม 1 รายการ
-colors.extend(["orange","yellow"])
-print(colors)
+# colors=["red","green","blue","black","white"]
+# colors.append("brown") #เพิ่ม 1 รายการ
+# colors.extend(["orange","yellow"])
+# colors.insert(1,"gray")
+# colors.remove("blue")
+# #colors.clear()
+# colors.sort()
+# colors.reverse()
+# print(colors)
+
+# #Tuple
+# product=("plant",150.0,10)
+# #(name,price,stock)=product
+# # print(product[2])
+# # print(product[0])
+# # print(len(product))
+# #print(price)
+# for item in product:
+#     print(item)
+# colors=("red","green","blue")
+# colors2=tuple(("red","black","white"))
+# fullcolors=colors+colors2
+# # print(fullcolors)
+# # print(type(fullcolors))
+# # print(len(fullcolors))
+# # power=colors*2
+# # print(power)
+# # print(fullcolors[2:])
+# print(colors.index("blue"))
+# print(fullcolors.count("red"))
+
+# # #How to use set
+# animals={"bird","dog","Lion","tiger",100,True}
+# animals.add("duck")
+# animals.update(("rabbit","Zebra"))
+# pet={"dog","cat","rabbit"}
+# # data=animals.union(pet)
+# data=animals.difference(pet)
+# # print(len(animals))
+# # print(type(animals))
+# # for item in animals:
+# #     print(item)
+# # print("frog"in animals)
+
+# print(data)
+
+#Dictionary
+colors={
+    "red":"แดง",
+    "green":"เขียว",
+    "blue":"ฟ้า"
+}
+colors["yellow"]="เหลือง"
+colors["blue"]="คราม"
+
+confirms={
+    True:"ตกลง",
+    False:"ยกเลิก"
+}
+
+month={
+    1:"มกราคม",
+    2:"Feb",
+    3:"March"
+}
+
+number={
+    "เลขคู่":[2,4,6,8,10],
+    "เลขคี่":[1,3,5,7,9]
+}
+# print(colors["green"])
+# print(colors["yellow"])
+# print(type(colors))
+# print(len(colors))
+print(number["เลขคู่"])
