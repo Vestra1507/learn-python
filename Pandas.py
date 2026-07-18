29c3f58517db6136550831dfefd17dfc7603319f
Rachata Sangeamjai <Rachata.sang@outlook.com>
2026-07-18 22:30:46 +0700

    update

---
+++ Pandas.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+import numpy as np
+
+# สร้างข้อมูลจาก Pandas 
+# data_ls =  [1,2,"Vistra",True]  #create data type list
+# ps = pd.Series(data_ls) #กำหนดตัวแปรสำหรับเก็บข้อมูล ที่สร้างข้อมูลเป็น series จาก Pandas 
+# #print(ps)
+
+# data_tp = (10,20,30,"Python","Kanompang") #create data type tuple
+# pa = pd.Series(data_tp) #กำหนดตัวแปรสำหรับเก็บข้อมูล ที่สร้างข้อมูลเป็น series จาก Pandas 
+
+# print(pa)
+
+# # สร้าง series จาก Numpy 
+# ndata=np.array([10,20,"Vistra",True])
+# pdsr = pd.Series(ndata)
+# print(pdsr)
+
+# # กำหนด ค่า Index เอง
+# item = [10,15,"Vistra","Pee"]
+# idx = [4,8,12,16]
+# pd_item = pd.Series(item,index=idx)
+# print(pd_item)
+
+# #สร้าง series จาก Dictionary
+# data_dict = {"green":"เขียว","red":"แดง"}
+# dict = pd.Series(data_dict)
+# print(dict)
+
+
